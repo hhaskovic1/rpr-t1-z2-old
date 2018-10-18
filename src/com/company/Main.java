5b38ec2c3672ccb11556d6733e1df9aa57414ac3
student <hhaskovic1@etf.unsa.ba>
2018-10-18 09:50:11 +0200

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
+package com.company;
 import java.util.Scanner;
-package ;
+
 
 public class Main {
 
@@ -9,5 +10,18 @@ public class Main {
         System.out.println("Unesite broj");
         int n = scanner.nextInt();
 
+        for(int i=2; i<n; i++) {
+            if(i% sumaCifara(i) == (0)) System.out.println(i);
+        }
+
+    }
+
+    public static int sumaCifara(int n) {
+        int suma = 0;
+        while(n!=0) {
+            suma += n % 10;
+            n /= 10;
+        }
+        return suma;
     }
 }
