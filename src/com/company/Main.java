06796a354fe05bee489162c7fd034a2224c3f2ba
student <hhaskovic1@etf.unsa.ba>
2018-10-18 09:38:12 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+package ;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite broj");
+        int n = scanner.nextInt();
+
+    }
+}
